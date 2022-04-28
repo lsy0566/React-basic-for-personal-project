d4ede470835ff6020ba8ae24f186b2a1d65b8c42
dong wook Lee <ehdrnr12@nate.com>
2022-04-28 22:24:32 +0900

    React tutorial practice

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/main.scss'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import registerServiceWorker from './registerServiceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// registerServiceWorker();
 root.render(
   <React.StrictMode>
     <App />
